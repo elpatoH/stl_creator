d34847caad2303bf91f3567bc9adbb5d9016d68a
elpatoHacker <daniel.urias.gil@gmail.com>
2023-04-18 23:34:56 -0700

    started add_sphere

--- 3d.c
+++ 3d.c
@@ -9,6 +9,8 @@ files by using various methods
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <stdint.h>
+#include <math.h>
 
 Scene3D *Scene3D_create()
 {
@@ -377,12 +379,182 @@ void Scene3D_add_quadrilateral(Scene3D *scene, Coordinate3D a, Coordinate3D b, C
     Scene3D_add_triangle(scene, triangle_4);
 }
 
+/// @brief 
+/// @param point 
+/// @param file 
+void write_point_binary(Coordinate3D point, FILE* file){
+    float points[] = {point.x, point.y, point.z};
+    fwrite(points, sizeof(float), 3, file);
+    fflush(file);
+}
+
+/// @brief 
+/// @param target_shape 
+/// @param file 
+/// @param node 
+/// @param cur_shape 
+void find_triangle_binary(int target_shape, FILE* file, Triangle3DNode* node, int cur_shape){
+    if (cur_shape == target_shape){
+        write_point_binary(node->triangle.a, file);
+        write_point_binary(node->triangle.b, file);
+        write_point_binary(node->triangle.c, file);
+    }
+    else{
+        find_triangle_binary(target_shape, file, node->next, ++cur_shape);
+    }
+}
+
 void Scene3D_write_stl_binary(Scene3D *scene, char *file_name)
 {
+    //open file
+    FILE* file = fopen(file_name, "w");
+    if (file == NULL) {
+        fprintf(stderr, "Opening file failed with code %d.\n", errno);
+        return;
+    }
+
+    //start file 80 bytes of zeroed memory
+    void* buffer = calloc(80, 1);
+    fwrite(buffer, 80, 1, file);
+    fflush(file);
+    
+    //write number of triangles
+    uint32_t my_uint32 = scene->count;
+    fwrite(&my_uint32, sizeof(uint32_t), 1, file);
+
+    //write triangles
+    int amount_shapes = scene->count;
+    for (int target_shape = 0; target_shape < amount_shapes; target_shape++){ 
+        float facet = 0.0;
+        fwrite(&facet, sizeof(facet), 1, file);
+        fwrite(&facet, sizeof(facet), 1, file);
+        fwrite(&facet, sizeof(facet), 1, file);
+
+        find_triangle_binary(target_shape, file, scene->root, 0);
+
+        //last two bytes
+        uint16_t end = 0;
+        fwrite(&end, sizeof(uint16_t), 1, file);
+        fflush(file);
+    }
+
+    //end file
+    fflush(file);
+    fclose(file);
+}
+
+/// @brief 
+/// @param radius 
+/// @param phi 
+/// @return 
+double sphere_to_car_z(double radius, double phi){
+    // convert angles to radians
+    double phi_radians = phi * PI / 180.0; 
+    return (radius * cos(phi_radians));
+}
+
+/// @brief 
+/// @param radius 
+/// @param theta 
+/// @param phi 
+/// @return 
+double sphere_to_car_y(double radius, double theta, double phi){
+    // convert angles to radians
+    double theta_radians = theta * PI / 180.0; 
+    double phi_radians = phi * PI / 180.0; 
+    return (radius * sin(phi_radians) * sin(theta_radians));
+}
+
+/// @brief 
+/// @param radius 
+/// @param theta 
+/// @param phi 
+/// @return 
+double sphere_to_car_x(double radius, double theta, double phi){
+    // convert angles to radians
+    double theta_radians = theta * PI / 180.0; 
+    double phi_radians = phi * PI / 180.0; 
+    return (radius * sin(phi_radians) * cos(theta_radians));
+}
+
+/// @brief 
+/// @param radius 
+/// @param theta 
+/// @param phi 
+/// @return 
+double* sphere_cords_to_cartesian(double radius, double theta, double phi){
+    double* cords = malloc(3);
+    cords[0] = sphere_to_car_x(radius, theta, phi);
+    cords[1] = sphere_to_car_y(radius, theta, phi);
+    cords[2] = sphere_to_car_z(radius, phi);
+    return cords;
+}
+
+double roundToDecimalPlaces(double value, int decimalPlaces) {
+    double multiplier = pow(10, decimalPlaces);
+    double rounded = round(value * multiplier) / multiplier;
+    double roundedUp = ceil(value * multiplier) / multiplier;
+    double roundedDown = floor(value * multiplier) / multiplier;
+    double diffUp = roundedUp - value;
+    double diffDown = value - roundedDown;
+    if (diffUp < 0.5 / multiplier && diffDown < 0.5 / multiplier) {
+        if (diffUp < diffDown) {
+            return roundedUp;
+        } else {
+            return roundedDown;
+        }
+    } else if (diffUp < 0.5 / multiplier) {
+        return roundedUp;
+    } else if (diffDown < 0.5 / multiplier) {
+        return roundedDown;
+    } else {
+        return rounded;
+    }
 }
 
 void Scene3D_add_sphere(Scene3D *scene, Coordinate3D origin, double radius, double increment)
 {
+    for (double phi = increment; phi <= 180; phi += increment){
+        for (double theta = 0; theta < 360; theta += increment){
+            /*
+            Convert the following four spherical coordinates to cartesian coordinates (x, y, z):
+                ● (radius, theta, phi)
+                ● (radius, theta, phi - increment)
+                ● (radius, theta - increment, phi)
+                ● (radius, theta - increment, phi - increment)
+            */
+           double* cord1 = sphere_cords_to_cartesian(radius, theta, phi);
+           double* cord2 = sphere_cords_to_cartesian(radius, theta, phi - increment);
+           double* cord3 = sphere_cords_to_cartesian(radius, theta - increment, phi);
+           double* cord4 = sphere_cords_to_cartesian(radius, theta - increment, phi - increment);
+
+           double value1 = 0.00015;
+
+           double perro = roundToDecimalPlaces(value1, 4);
+
+           printf("wtf: %.4f\n", perro);
+           printf("coordinates: (%.5f,%.5f,%.5f)\n", roundToDecimalPlaces(cord1[0], 4), roundToDecimalPlaces(cord1[0], 4), roundToDecimalPlaces(cord1[0], 4));
+           printf("coordinates: (%.5f,%.5f,%.5f)\n", roundToDecimalPlaces(cord2[0], 4), roundToDecimalPlaces(cord2[0], 4), roundToDecimalPlaces(cord2[0], 4));
+           printf("coordinates: (%.5f,%.5f,%.5f)\n", roundToDecimalPlaces(cord3[0], 4), roundToDecimalPlaces(cord3[0], 4), roundToDecimalPlaces(cord3[0], 4));
+           printf("coordinates: (%.5f,%.5f,%.5f)\n\n", roundToDecimalPlaces(cord4[0], 4), roundToDecimalPlaces(cord4[0], 4), roundToDecimalPlaces(cord4[0], 4));
+           /*
+           Place a quadrilateral between these four cartesian coordinates
+                ● Coordinates should be rounded to four decimal places using standard
+                mathematical rounding. For example, 0.00015 rounds to 0.0002, and 0.00014
+                rounds to 0.0001.
+                ● When dealing with these coordinates, keep in mind to offset everything by the
+                origin (x, y, z) values
+            */
+
+           fflush(stdout);
+           free(cord1);
+           free(cord2);
+           free(cord3);
+           free(cord4);
+        }
+    }
+    double x = atan2(2, 1);
+    printf("double: %f", x);
 }
 
 void Scene3D_add_fractal(Scene3D *scene, Coordinate3D origin, double size, int levels)
